aa170a1447a16b712d9ed7b649be2048e2a067fa
Herb Stahl <ghstahl@gmail.com>
2021-09-14 09:56:44 -0700

    added injection by reflection (#2)
    * added injection by reflection
    
    * Update README.md
    
    * added safe inject option
    
    * unit tests on how to add a map and slice type
    
    * added calling Ctor method on a type if it exists
    
    * bump
    
    * remove func

--- containerGetter_test.go
+++ containerGetter_test.go
@@ -2,6 +2,7 @@ package di
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"sync"
 	"sync/atomic"
@@ -90,6 +91,7 @@ func TestGetByType_FromSubContainer(t *testing.T) {
 		},
 		Unshared: true,
 	})
+	assert.NoError(t, err)
 	err = b.Add(Def{
 		Type:             reflect.TypeOf(&mockObject2{}),
 		ImplementedTypes: types,
@@ -101,13 +103,14 @@ func TestGetByType_FromSubContainer(t *testing.T) {
 		},
 		Unshared: true,
 	})
+	assert.NoError(t, err)
 	var app = b.Build()
 
 	_, err = app.SafeGetByType(rt)
-	require.NotNil(t, err)
+	require.Error(t, err)
 
 	_, err = app.SafeGetManyByType(rt)
-	require.NotNil(t, err)
+	require.Error(t, err)
 
 	sub, err := app.SubContainer()
 	require.Nil(t, err)
@@ -776,4 +779,157 @@ func TestConcurrentBuild(t *testing.T) {
 	app.Delete()
 
 	require.Equal(t, uint64(1), atomic.LoadUint64(&numClose))
+}
+
+func TestTypedObjects_map_type(t *testing.T) {
+	type MyMapType map[string]string
+	rtPtr := reflect.TypeOf(&MyMapType{})
+	rtElem := rtPtr.Elem()
+
+	b, _ := NewBuilder()
+	b.Add(Def{
+		Type:     rtPtr,
+		Unshared: true,
+		Build: func(ctn Container) (interface{}, error) {
+			result := MyMapType{
+				"test": "dog",
+			}
+			return &result, nil
+		},
+	})
+	var app = b.Build()
+	obj := app.GetByType(rtElem).(*MyMapType)
+	assert.NotNil(t, obj)
+	dObj := *obj
+
+	assert.Equal(t, "dog", dObj["test"])
+}
+
+func TestTypedObjects_slice_type(t *testing.T) {
+	type MySliceType []string
+	rtPtr := reflect.TypeOf(&MySliceType{})
+	rtElem := rtPtr.Elem()
+
+	b, _ := NewBuilder()
+	b.Add(Def{
+		Type:     rtPtr,
+		Unshared: true,
+		Build: func(ctn Container) (interface{}, error) {
+			result := MySliceType{
+				"dog",
+			}
+			return &result, nil
+		},
+	})
+	var app = b.Build()
+	obj := app.GetByType(rtElem).(*MySliceType)
+	assert.NotNil(t, obj)
+	dObj := *obj
+
+	assert.Equal(t, "dog", dObj[0])
+}
+
+func TestTypedObjects_ReflectBuilder_panic(t *testing.T) {
+	b, _ := NewBuilder()
+	b.Add(Def{
+		Type:     reflect.TypeOf(&mockObjectDependencyDoesNotExist{}),
+		Unshared: true,
+	})
+	var app = b.Build()
+	assert.Panics(t, func() {
+		rt := reflect.TypeOf(&mockObjectDependencyDoesNotExist{}).Elem()
+		app.GetByType(rt)
+	})
+}
+
+func TestTypedObjects_ReflectBuilder_panic_must_not(t *testing.T) {
+	b, _ := NewBuilder()
+	b.Add(Def{
+		Type:       reflect.TypeOf(&mockObjectDependencyDoesNotExist{}),
+		Unshared:   true,
+		SafeInject: true,
+	})
+	var app = b.Build()
+
+	rt := reflect.TypeOf(&mockObjectDependencyDoesNotExist{}).Elem()
+	obj := app.GetByType(rt).(*mockObjectDependencyDoesNotExist)
+	assert.NotNil(t, obj)
+	assert.Nil(t, obj.NotHere)
+}
+func TestTypedObjects_ReflectBuilder_Obj_with_CTOR(t *testing.T) {
+	rtPtr := reflect.TypeOf(&mockObjectWithCtor{})
+	rtElem := rtPtr.Elem()
+	fmt.Printf("rtPtr=%v, rtElem=%v", rtPtr, rtElem)
+
+	method, ok := rtPtr.MethodByName("Ctor")
+	assert.True(t, ok)
+	fmt.Printf("method=%v", method)
+
+	b, _ := NewBuilder()
+	b.Add(Def{
+		Type:       rtPtr,
+		Unshared:   true,
+		SafeInject: true,
+	})
+	var app = b.Build()
+
+	rt := reflect.TypeOf(&mockObjectWithCtor{}).Elem()
+	obj := app.GetByType(rt).(*mockObjectWithCtor)
+	assert.NotNil(t, obj)
+	assert.True(t, obj.CtorCalled)
+}
+
+func TestTypedObjects_ReflectBuilder_ManyAdded_OneRetrieved(t *testing.T) {
+	b, _ := NewBuilder()
+	types := NewTypeSet()
+	rt := GetInterfaceReflectType((*IGetterSetter)(nil))
+	types.Add(rt)
+	// Add 2 of the same type
+	b.Add(Def{
+		Type:             reflect.TypeOf(&mockObject2{}),
+		ImplementedTypes: types,
+		Build: func(ctn Container) (interface{}, error) {
+
+			return &mockObject2{
+				Value: 2,
+			}, nil
+		},
+		Unshared: true,
+	})
+	b.Add(Def{
+		Type:             reflect.TypeOf(&mockObject2{}),
+		ImplementedTypes: types,
+		Build: func(ctn Container) (interface{}, error) {
+			return &mockObject2{
+				Value: 1,
+			}, nil
+		},
+		Unshared: true,
+	})
+
+	b.Add(Def{
+		Type:     reflect.TypeOf(&mockObject3{}),
+		Unshared: true,
+	})
+	// The last object added
+
+	var app = b.Build()
+
+	// get the type of the object we want to retrieve
+	rt = reflect.TypeOf(&mockObject3{}).Elem()
+
+	obj1, err := app.SafeGetByType(rt)
+	require.Nil(t, err)
+
+	obj2, err := app.SafeGetByType(rt)
+	require.Nil(t, err)
+
+	// should retrieve different object every time
+	require.False(t, obj1 == obj2)
+
+	// value must be of the last one added
+	exected := 1
+	require.Equal(t, exected, obj1.(*mockObject3).GetterSetter.GetValue())
+	require.Equal(t, exected, obj2.(*mockObject3).GetterSetter.GetValue())
+
 }
